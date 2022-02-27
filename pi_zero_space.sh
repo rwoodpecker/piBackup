0d307c32061a01a79fb69a9c8d58e5921c8d72ba
rwoodpecker <rwoodpecker@users.noreply.github.com>
2022-02-27 12:54:05 +1100

    Update pi_zero_space.sh

--- pi_zero_space.sh
+++ pi_zero_space.sh
@@ -6,7 +6,6 @@ if [[ $REPLY =~ ^[Yy]$ ]]
 then
     time="$(date +%s)"
     zero_file=$HOME/zero_file
-    
     echo "Writing zeroes to pi at $(date)."
     dd if=/dev/zero of=${zero_file}
     echo "Completed writing zeroes; took $(($(date +%s)-time)) seconds. File size is $(ls -lh $zero_file | awk '{print  $5}')"
