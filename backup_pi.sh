1417d8489708088cde720626de0f8d68ccaa84f8
rwoodpecker <rwoodpecker@users.noreply.github.com>
2022-02-27 15:18:51 +1100

    improve the script.

--- backup_pi.sh
+++ backup_pi.sh
@@ -5,8 +5,7 @@ output_dir=$HOME
 # Find disk with linux partition (Raspbian)
 export disk=`diskutil list | grep "Linux" | sed 's/.*\(disk[0-9]\).*/\1/' | uniq`
 if [ $disk ]; then
-    echo $disk
-    echo $outpur_dir
+    echo "Backing up ${disk} to ${output_dir}."
 else
     echo "Disk not found."
     exit
@@ -17,14 +16,13 @@ if [ $# -eq 0 ] ; then
 else
     backup_name=$1
 fi
-backup_name+="backup"
-echo $backup_name
+backup_name+="_backup"
 
-echo "Backing up..."
+echo "Beginning backup, this will take some time..."
 diskutil unmountDisk /dev/$disk
-time sudo dd if=/dev/r$disk bs=4m | gzip -9 > $output_dir/pibackup.img.gz
+time sudo dd if=/dev/r$disk bs=4m | gzip -9 > $output_dir/{backup_name}_temp.img.gz
 
 #rename to the current date
 echo "Finished copying backup, renaming file..."
-mv -n $output_dir/piback.img.gz $output_dir/$backup_name`date +%Y%m%d`.img.gz
+mv -n $output_dir/{backup_name}_temp.img.gz $output_dir/$backup_name`date +%Y%m%d`.img.gz
 echo "Finished copying backup to ${output_dir}/${backup_name}`date +%Y%m%d`.img.gz"
